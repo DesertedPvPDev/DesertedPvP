b35f25e0ba9c1527ee3cad26b0248ac6ec827c19
xXXIMMATTHEWXXx <mattparksjr@gmail.com>
2018-07-25 20:18:22 -0400

    Fix that NPE

--- src/main/java/codes/matthewp/desertedpvp/event/interact/InteractEvent.java
+++ src/main/java/codes/matthewp/desertedpvp/event/interact/InteractEvent.java
@@ -3,9 +3,7 @@ package codes.matthewp.desertedpvp.event.interact;
 import codes.matthewp.desertedpvp.data.Messages;
 import codes.matthewp.desertedpvp.killstreak.KillStreaks;
 import codes.matthewp.desertedpvp.kit.KitSelector;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -23,10 +21,10 @@ public class InteractEvent implements Listener {
             if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
                 KillStreaks.openGUI(e.getPlayer());
             }
-        } else if(e.getAction() == Action.RIGHT_CLICK_BLOCK || e.getClickedBlock().getType() == Material.CHEST) {
-            if(e.getClickedBlock().hasMetadata("isSupplyCrate")) {
+        } else if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock() != null && e.getClickedBlock().getType() == Material.CHEST) {
+            if (e.getClickedBlock().hasMetadata("isSupplyCrate")) {
                 String ownerUUID = e.getClickedBlock().getMetadata("isSupplyCrate").get(0).asString();
-                if(!e.getPlayer().getUniqueId().toString().equals(ownerUUID)) {
+                if (!e.getPlayer().getUniqueId().toString().equals(ownerUUID)) {
                     e.setCancelled(true);
                     e.getPlayer().sendMessage(Messages.getMessage("notYourDrop"));
                 }
